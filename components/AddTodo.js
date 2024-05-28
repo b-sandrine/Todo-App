8401776dbc1f99dcd15a983933607896871ea951
sandrine dushimimana <sanrdushimimana@gmail.com>
2024-05-28 08:16:54 +0200

    Project Initialized

---
+++ components/AddTodo.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+
+function AddToDO() {
+  return (
+    <View>
+      <Text>Add To Do Page</Text>
+    </View>
+  )
+}
+
+export default AddToDO
---
+++ components/TodoItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { View } from 'react-native'
+
+function ToDoItem() {
+  return (
+    <View>
+      <Text>To Do Item page</Text>
+    </View>
+  )
+}
+
+export default ToDoItem
